8adfed291745116e499981950db1ae5606b2cf7d
BlueEagle421 <joachim.wielewski@gmail.com>
2024-03-17 11:22:49 +0100

    Remove old code

--- Source/LuciferiumExpansion/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace LuciferiumExpansion
-{
-    internal class CompProperties_LuciferiumPump : CompProperties
-    {
-        public CompProperties_LuciferiumPump() => this.compClass = typeof(CompLuciferiumPump);
-    }
-}
