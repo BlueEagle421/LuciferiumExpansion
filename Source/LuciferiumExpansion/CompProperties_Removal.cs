451df1daf7859b94d5d62edc691d5ac90ef03df2
BlueEagle421 <joachim.wielewski@gmail.com>
2024-03-17 10:32:15 +0100

    Remove old code

--- Source/LuciferiumExpansion/CompProperties_Removal.cs
+++
@@ -1,9 +0,0 @@
-﻿using Verse;
-
-namespace LuciferiumExpansion
-{
-    public class CompProperties_Removal : CompProperties
-    {
-        public CompProperties_Removal() => this.compClass = typeof(CompRemoval);
-    }
-}
