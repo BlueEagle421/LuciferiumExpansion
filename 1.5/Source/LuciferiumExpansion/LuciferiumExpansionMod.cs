876f278e1d64028d4c2c05338563b885c35c8901
BlueEagle421 <joachim.wielewski@gmail.com>
2024-05-03 11:50:05 +0200

    Add collecting speed setting

---
+++ 1.5/Source/LuciferiumExpansion/LuciferiumExpansionMod.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace LuciferiumExpansion
+{
+    public class LuciferiumExpansionMod : Mod
+    {
+        public static LuciferiumExpansionSettings Settings { get; private set; }
+        public LuciferiumExpansionMod(ModContentPack content) : base(content)
+        {
+            Settings = GetSettings<LuciferiumExpansionSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            //Begin
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+
+            //CollectionSpeedMultiplier
+            listingStandard.Label("USH_LE_CollectionSpeedLabel".Translate());
+            float severitySliderValue = listingStandard.Slider(Settings.CollectionSpeedMultiplier, 0.01f, 2.5f);
+            listingStandard.Label("USH_LE_CollectionSpeedDesc".Translate(severitySliderValue.ToStringPercent()));
+            Settings.CollectionSpeedMultiplier = severitySliderValue;
+
+            //End
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory() => "Luciferium Expansion";
+    }
+    public class LuciferiumExpansionSettings : ModSettings
+    {
+        public float CollectionSpeedMultiplier = 1f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref CollectionSpeedMultiplier, "USH_CollectionSpeedMultiplier", 1f);
+        }
+    }
+}
