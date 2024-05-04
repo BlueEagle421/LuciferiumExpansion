daaa183b059bc5b8c03089552261523d223d3501
BlueEagle421 <joachim.wielewski@gmail.com>
2024-05-04 17:00:07 +0200

    Add brain destroying to last wish

---
+++ 1.5/Source/LuciferiumExpansion/HediffCompDestroyBrainOnDeath.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+
+namespace LuciferiumExpansion
+{
+    public class HediffCompProperties_DestroyBrainOnDeath : HediffCompProperties
+    {
+        public HediffCompProperties_DestroyBrainOnDeath() => compClass = typeof(HediffCompDestroyBrainOnDeath);
+    }
+
+    public class HediffCompDestroyBrainOnDeath : HediffComp
+    {
+        public HediffCompProperties_DestroyBrainOnDeath Props => (HediffCompProperties_DestroyBrainOnDeath)props;
+
+        public override void Notify_PawnDied(DamageInfo? dinfo, Hediff culprit = null)
+        {
+            base.Notify_PawnDied(dinfo, culprit);
+
+            RemoveBrain();
+        }
+
+        private void RemoveBrain()
+        {
+            BodyPartRecord brain = Pawn.health.hediffSet.GetBrain();
+            if (brain == null)
+                return;
+
+            Hediff hediff = HediffMaker.MakeHediff(HediffDefOf.MissingBodyPart, Pawn, brain);
+            Pawn.health.AddHediff(hediff, brain, null, null);
+
+        }
+    }
+}
