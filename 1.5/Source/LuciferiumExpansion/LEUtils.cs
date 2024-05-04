abafad8471b5c1336e04be103e26c1b78e9c494f
BlueEagle421 <joachim.wielewski@gmail.com>
2024-05-04 17:00:43 +0200

    Add low luci warning

--- 1.5/Source/LuciferiumExpansion/LEUtils.cs
+++ 1.5/Source/LuciferiumExpansion/LEUtils.cs
@@ -6,9 +6,9 @@ namespace LuciferiumExpansion
         //the amount of scarlet sludge needed to sustain one addict for a year
         //10 (luci needed per year) * 24 (mechanites per luci) * 48 (sludge per mechnite)
         public const float SUSTAIN_ONE_YEAR = 11520f;
+        public const float WARNING_PERCENT = 0.1f;
 
         public static float ConvertToLuciferium(this float sludge) => sludge / 24f / 48f;
-
 
     }
 }
--- 1.5/Source/LuciferiumExpansion/ScarletSludgeManager.cs
+++ 1.5/Source/LuciferiumExpansion/ScarletSludgeManager.cs
@@ -1,3 +1,4 @@
+using RimWorld;
 using RimWorld.Planet;
 using Verse;
 
@@ -5,13 +6,22 @@ namespace LuciferiumExpansion
 {
     public class ScarletSludgeManager : WorldComponent
     {
-
+        private bool _shouldWarn = true;
         private float _scarletSludgeAmount = -1;
 
         public float ScarletSludgeAmount
         {
             get => _scarletSludgeAmount;
-            set => _scarletSludgeAmount = value <= 0 ? 0 : value;
+            set
+            {
+                if (value > _scarletSludgeAmount)
+                    _shouldWarn = true;
+
+                if (value <= LEUtils.WARNING_PERCENT * DefaultScarletSludgeAmount())
+                    DoWarning();
+
+                _scarletSludgeAmount = value <= 0 ? 0 : value;
+            }
         }
         public static ScarletSludgeManager Instance { get; private set; }
 
@@ -27,8 +37,22 @@ namespace LuciferiumExpansion
         {
             base.ExposeData();
             Scribe_Values.Look(ref _scarletSludgeAmount, "USH_ScarletSludgeAmount");
+            Scribe_Values.Look(ref _shouldWarn, "USH_DidWarning", true);
         }
 
-        private float DefaultScarletSludgeAmount() => LEUtils.SUSTAIN_ONE_YEAR * 8f;
+        private void DoWarning()
+        {
+            if (!_shouldWarn)
+                return;
+
+            Find.LetterStack.ReceiveLetter(
+                "USH_LE_ScarletWarningLetterLabel".Translate(),
+                "USH_LE_ScarletWarningLetterDesc".Translate(),
+                LetterDefOf.ThreatSmall);
+
+            _shouldWarn = false;
+        }
+
+        public float DefaultScarletSludgeAmount() => LEUtils.SUSTAIN_ONE_YEAR * 8f;
     }
 }
